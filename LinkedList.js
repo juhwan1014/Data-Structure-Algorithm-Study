a004107ef78d5b8d1952b942ad95b23fc3179224
juhwan1014 <jmoon63@my.bcit.ca>
2021-11-28 21:06:11 -0800

    add linkedList

---
+++ LinkedList.js
@@ -0,0 +1,14 @@
+class Node {
+    constructor(val, next = null){
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+
+}
