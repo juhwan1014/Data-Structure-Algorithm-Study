ee3e9307b48fb1e22d550261313e3737075f4929
Juhwan Moon <juhwan1014@naver.com>
2021-12-31 18:47:07 -0800

    Time: 194 ms (34.34%), Space: 49.7 MB (43.98%) - LeetHub

--- find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
+++ find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
@@ -2,29 +2,64 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var findDisappearedNumbers = function(nums) {
+// var findDisappearedNumbers = function(nums) {
     
-  let map = {}
+//   let map = {}
 
-    for(let i = 0; i < nums.length; i++){
-      if(!map[nums[i]]) {
-          map[nums[i]] = true
-      } 
-  }
+//     for(let i = 0; i < nums.length; i++){
+//       if(!map[nums[i]]) {
+//           map[nums[i]] = true
+//       } 
+//   }
     
-    // map ={1: true, 2:true , 3:true, 4:true, 7:true ,8:true}
+//     // map ={1: true, 2:true , 3:true, 4:true, 7:true ,8:true}
     
-    for(let i = 1; i <= nums.length; i++){
-       if(i in map) 
-          delete map[i]
-       else      
-           map[i] = false
-    }
+//     for(let i = 1; i <= nums.length; i++){
+//        if(i in map) 
+//           delete map[i]
+//        else      
+//            map[i] = false
+//     }
     
-return Object.keys(map)
+// return Object.keys(map)
   
     
-};
+// };
+
+
+var findDisappearedNumbers = (nums) => {
+    let map = {}
+    
+    for(let i = 0; i < nums.length; i++){
+        if(!map[nums[i]]){
+            map[nums[i]] = true
+        }
+    }
+    
+  for(let i = 1; i <= nums.length; i++){
+      if(i in map){
+          delete map[i]
+      } else
+          map[i] = false
+  }
+   
+    return Object.keys(map)
+   
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
