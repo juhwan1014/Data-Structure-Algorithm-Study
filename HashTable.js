ea4e2672c3187ad0b9d72ee22661ff30f8f4eb47
juhwan1014 <jmoon63@my.bcit.ca>
2022-01-24 23:04:59 -0800

    Hash table concept

--- HashTable.js
+++ HashTable.js
@@ -11,4 +11,9 @@
 // Firstly, we can generate Hash code with key and Hash function, and the Hash code can be index of Array.
 // which means the hash codes are integer, and they are designated fixed array space.
 
-// So… 	it is possible to access directly with hashcode.
+// So… 	it is possible to access directly with hashcode, and that's why the hash map's searching speed is really fast. so,,, the time complexity is O(1) in the most cases. 
+
+// However, there were some exceptions. The reason is because of colligion. 
+// Even if we have different key values, the same hash code can be created with them. Or, even if hash codes are different, it is possible to be allocated in the same index room.
+
+// So, in worst case, the time complexity could be O(n)
