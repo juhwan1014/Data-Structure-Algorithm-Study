8e423581d98360efe25a28c7053a8469c5d8bf5e
juhwan1014 <jmoon63@my.bcit.ca>
2022-01-24 22:59:18 -0800

    hash table

---
+++ HashTable.js
@@ -0,0 +1,14 @@
+
+// Hash Table has a lot of dther names.
+// maps/ hash maps/ unordered maps/ dictionary/ object  end so on
+
+// It is consist of key and value, so every single key could be mappinig with specific value.
+
+// Also, there are some internal components. Hash codes, Hash function and Array. 
+
+// Internally, Hash table has to be uesd Array. 
+
+// Firstly, we can generate Hash code with key and Hash function, and the Hash code can be index of Array.
+// which means the hash codes are integer, and they are designated fixed array space.
+
+// So… 	it is possible to access directly with hashcode.
