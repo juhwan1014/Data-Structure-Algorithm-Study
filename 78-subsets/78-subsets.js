62f23a1529b36a4a03cb05a695ec38ff7979eb62
Juhwan Moon <juhwan1014@naver.com>
2022-10-03 21:07:00 -0700

    Time: 123 ms (13.17%), Space: 44 MB (45.76%) - LeetHub

--- 78-subsets/78-subsets.js
+++ 78-subsets/78-subsets.js
@@ -16,16 +16,31 @@
 //     return result;
 // };
 
-function subsets(nums) {
-	const result = [];
-	dfs([], 0);
+// function subsets(nums) {
+// 	const result = [];
+// 	dfs([], 0);
 
-	function dfs(slate, index) {
-		result.push(slate);
-		for (let i = index; i < nums.length; i++) {
-			dfs([...slate, nums[i]], i + 1);
-		}
-	}
+// 	function dfs(slate, index) {
+// 		result.push(slate);
+// 		for (let i = index; i < nums.length; i++) {
+// 			dfs([...slate, nums[i]], i + 1);
+// 		}
+// 	}
 
-	return result;
-}
+// 	return result;
+// }
+
+var subsets = function(nums) {
+    function findSubset(arr,curr){
+            res.push([...curr])
+        for(let i=0;i<arr.length;i++){
+            curr.push(arr[i])
+            findSubset(arr.slice(i+1),curr)
+            curr.pop()
+        }
+    }
+    
+    let res = []
+    findSubset(nums,[])
+    return res
+};
